3439ee58dd100a3b19ff0a068308fb17bacf256b
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-12-24 22:12:38 +0900

    feat: create markdown sample

--- src/components/MarkdownSample.vue
+++ src/components/MarkdownSample.vue
@@ -1,45 +1,116 @@
 <scrpit lang="ts">
-export default { name: 'App' }
+export default { name: 'MarkdownSample' }
 </scrpit>
 
 <script setup lang="ts">
-import { NTag } from 'naive-ui'
-import { ref } from 'vue'
-import Markdown from 'vue3-markdown-it'
-import Editor from '@tinymce/tinymce-vue'
+import MarkdownIt from 'markdown-it'
+import { ref, watch } from 'vue'
 
-const title = '喵玉殿'
+const emit = defineEmits<{
+  (event: 'submit', value: string): void
+}>()
+
+const markdown = new MarkdownIt()
 const source = ref<string>('')
+const result = ref<string>(markdown.render(source.value))
+
+watch(source, (): void => {
+  result.value = markdown.render(source.value)
+})
+
+enum DisplayModeEnum {
+  WRITE = 'write',
+  REVIEW = 'review',
+}
+
+const displayMode = ref<DisplayModeEnum>(DisplayModeEnum.WRITE)
 </script>
 
 <template>
-  <div class="app">
-    <NTag>{{ title }}</NTag>
+  <div class="article">
+    <div class="button-group">
+      <button
+        class="button"
+        :class="{ current: displayMode === DisplayModeEnum.WRITE }"
+        @click="displayMode = DisplayModeEnum.WRITE"
+      >
+        write
+      </button>
 
-    <div class="article">
-      <textarea v-model="source" />
-      <Markdown class="markdown" :source="source" />
+      <button
+        class="button"
+        :class="{ current: displayMode === DisplayModeEnum.REVIEW }"
+        @click="displayMode = DisplayModeEnum.REVIEW"
+      >
+        review
+      </button>
     </div>
 
-    <div class="article" style="margin: 2rem">
-      <Editor />
-    </div>
+    <textarea
+      class="write"
+      v-show="displayMode === DisplayModeEnum.WRITE"
+      v-model="source"
+    />
+
+    <div
+      class="review"
+      v-show="displayMode === DisplayModeEnum.REVIEW"
+      v-html="result"
+    />
+
+    <button
+      class="button submit"
+      :class="{
+        'submit-fix': displayMode === DisplayModeEnum.WRITE,
+      }"
+      @click="() => emit('submit', result)"
+    >
+      submit
+    </button>
   </div>
 </template>
 
 <style scoped>
 .article {
-  background: #eee;
+  box-sizing: border-box;
+  padding: 1rem;
+}
+
+.button-group {
   display: flex;
 }
 
-.article textarea {
-  height: 300px;
-  padding: 1rem;
-  width: 50vw;
+.button {
+  height: 2rem;
+  width: 5rem;
 }
 
-.article .markdown {
+.current {
+  opacity: 0.6;
+}
+
+.article .write {
+  border: 2px solid #333;
+  box-sizing: border-box;
+  height: 15rem;
+  margin: 0.5rem 0 0;
+  width: 100%;
+}
+
+.article .review {
+  border: 2px solid #333;
+  box-sizing: border-box;
+  height: 15rem;
+  margin: 0.5rem 0 0;
   padding: 0 1rem;
+  width: 100%;
+}
+
+.submit {
+  margin: 0.5rem 0 0;
+}
+
+.submit-fix {
+  margin: 3px 0 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Floor from '@/features/floor/components/Floor.vue'
 import Tiptap from '@/components/Tiptap.vue'
+import MarkdownSample from '@/components/MarkdownSample.vue'
 
 import type { FloorTypes } from '@/features/floor/types'
 
-const floors: FloorTypes.FloorInfo[] = [
+import { ref } from 'vue'
+
+const floors = ref<FloorTypes.FloorInfo[]>([
   {
     icon: '',
     username: 'Lv.1 大毛玉',
@@ -22,16 +25,19 @@ const floors: FloorTypes.FloorInfo[] = [
     floorNumber: 2,
     attachment: [],
   },
+])
 
-  {
-    icon: '',
-    username: 'Lv.3 大毛玉',
-    article:
-      '　　历史把那些为共同目标工作因而自己变得高尚的人称为最伟大的人物；经验赞美那些为大多数人带来幸福的人是最幸福的人；宗教本身也教诲我们，人人敬仰的典范，就曾为人类而牺牲自己──有谁敢否定这类教诲呢？\n　　如果我们选择了最能为人类而工作的职业，那么，重担就不能把我们压倒，因为这是为大家作出的牺牲；那时我们所享受的就不是可怜的、有限的、自私的乐趣，我们的幸福将属于千百万人，我们的事业将悄然无声地存在下去，但是它会永远发挥作用，而面对我们的骨灰，高尚的人们将洒下热泪。',
-    floorNumber: 3,
-    attachment: [],
-  },
-]
+function submit(article: string): void {
+  if (article) {
+    floors.value.push({
+      icon: '',
+      username: 'LV.1 毛玉',
+      article,
+      floorNumber: 0,
+      attachment: [],
+    })
+  }
+}
 </script>
 
 <template>
@@ -47,5 +53,6 @@ const floors: FloorTypes.FloorInfo[] = [
     />
   </div>
 
-  <Tiptap />
+  <!-- <Tiptap /> -->
+  <MarkdownSample @submit="submit" />
 </template>
